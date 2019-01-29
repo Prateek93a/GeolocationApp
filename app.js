845d82ac93e5c42a4037c4e825452a43961014ab
Prateek93a <44807945+Prateek93a@users.noreply.github.com>
2019-01-29 22:36:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require("express");
 const app=express();
 app.set("view-engine","ejs");
 app.use("/assets",express.static("assets"));
-app.get("https://prateek93a.github.io/",(req,res)=>{
+app.get("/",(req,res)=>{
     res.render("index.ejs");
 });
 app.listen(3000,"10.1.9.99",511,(err)=>{
