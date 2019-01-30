5a19019df4e6a5db268b5f104715a0491ac77869
Prateek93a <44807945+Prateek93a@users.noreply.github.com>
2019-01-30 08:10:08 +0530

    Delete myloc.js

--- myloc.js
+++
@@ -1,5 +0,0 @@
-window.onload=()=>{
-    navigator.geolocation.getCurrentPosition((position)=>{
-        document.getElementById("location").innerHTML="Your Location=>latitude:"+position.coords.latitude+" longitude:"+position.coords.longitude;
-    })
-}
